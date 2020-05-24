ef9b57db80af5a1c541643d3f07da2445e8aabc5
speedyjeff <speedyjeff@github.com>
2020-05-24 05:59:26 -0700

    update to close program after n ms

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,20 +9,23 @@ namespace Client
         public static int Main(string[] args)
         {
             var url = args.Length > 0 ? args[0] : "http://dl.google.com/googletalk/googletalk-setup.exe";
+            var stopAfter = args.Length > 1 ? Convert.ToInt32(args[1]) : -1;
             if (args.Length == 0)
             {
-                Console.WriteLine("./client <address>");
+                Console.WriteLine("./client <address> <stop after ms>");
                 Console.WriteLine($"    using default {url}");
             }
 
             // init
             var timer = new Stopwatch();
+            var duration = new Stopwatch();
             byte[] data;
 
             // start the watching thread
             Reader = new Thread(WaitForConsoleInput);
             Reader.Start();
 
+            duration.Start();
             while (System.Threading.Volatile.Read(ref IsSet) == 0)
             {
                 // get content
@@ -40,8 +43,15 @@ namespace Client
                 Console.WriteLine($"{DateTime.Now:o}\t{timer.Elapsed}\t{speed:f2}");
 
                 timer.Reset();
+
+                // check if we should stop
+                if (stopAfter > 0 && duration.ElapsedMilliseconds > stopAfter)
+                {
+                    break;
+                }
             }
 
+            Environment.Exit(0);
             return 0;
         }
 
